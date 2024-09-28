655131f6b4d1a623661e89306d9ecf952d0bb62f
unknown <jitender4451a@gmail.com>
2024-09-28 11:03:04 +0530

    initial commit

---
+++ notice.js
@@ -0,0 +1,50 @@
+const array = [{
+    mainDataInput:'Earn money',
+    datePass: '12-05-2025'
+}];
+
+function mainFrame(comboList){
+    let str = '';
+    
+    for(let i=0;i<array.length;i++){
+        let strAdd = '';
+         
+        console.log(array[i].mainDataInput);
+        strAdd= `
+            <div class='sizeFont'>➤ ${array[i].mainDataInput}</div>
+            <div class='sizeFont'>${array[i].datePass}</div>
+            <button onclick='
+                array.splice(${i}, 1);
+                mainFrame();' class='deleteStyle'>delete</button>
+        `;
+        
+        str = str + strAdd;
+        
+        
+        
+    }
+    document.querySelector('.display')
+        .innerHTML = `${str}`;
+        
+        
+}
+
+function addData(){
+    const dataInput= document.getElementById('areaValue')
+    const mainDataInput = dataInput.value;
+    let dat = document.getElementById('getDate');
+    let datePass=dat.value;
+    let comboList = {
+        mainDataInput: mainDataInput,
+        datePass: datePass
+    }
+    array.push(comboList);
+    
+    dataInput.value = '';
+    
+    console.log(array);
+    mainFrame(comboList);    
+}
+
+
+
